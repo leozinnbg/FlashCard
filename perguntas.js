a06d6e4c788741ba08b353f148f6c10e18c75cde
leozinnbg <140726203+leozinnbg@users.noreply.github.com>
2024-10-29 08:24:23 -0300

    zz

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Geografia',
+    'Quantos estados tem no Brasil?',
+    '26 e 1 distrito federal'
+)
+criaCartao(
+    'Matemática',
+    'Quantos Qual √3?',
+    '1,73'
+)
+criaCartao(
+    'História',
+    'Quantas fases tiveram na 1° guerra mundial? E quais são?',
+    '2, guerra de movimento e guerra das trincheiras'
+)
+criaCartao(
+    'Língua portuguesa',
+    'Quais são os Porque?',
+    'por que, porque, por quê, porquê'
+)
+criaCartao(
+    'química',
+    'O que são carbonetos?',
+    'compostos inorgânicos binários, classificados como cerâmicas, que contêm carbono'
+)
+criaCartao(
+    'Biologia',
+    'qual a porcentagem de Aa X aa?',
+    '50%'
+)
+criaCartao(
+    'Matemática II',
+    'O que é o section?',
+    'é a minha sessão'
+)
+criaCartao(
+    'Inglês',
+    'Tansforme em forma negativa do verbo "to be" no "simple future" e na forma completa a frase "I will be there for the concert.',
+    'I will not do there for the correct'
+)
+criaCartao(
+    'Ed. Fisíca',
+    'Qual o esporte mais tem pessoas em atividade?',
+    'Futebol'
+)
+criaCartao(
+    'Arte',
+    'Quais foram os artistas renacentistas?',
+    'Leonardo da Vinci, Michelangelo e Rafael'
+)
+criaCartao(
+    'Sociologia',
+    'Quem foi durkheim?',
+    'Durkheim foi um psicólogo, filósofo e sociólogo francês do século XIX'
+)
+criaCartao(
+    'Fisíca',
+    'O que siguinifica MRU?',
+    'Movimento Retilinio Uniforme'
+)
